c45330bda6030a6e3459d708b622731b798aa1a4
EC2 Default User <ec2-user@ip-172-31-75-199.ec2.internal>
2021-02-06 14:42:28 +0000

    [Finish]3-7 画像投稿機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_083816) do
+ActiveRecord::Schema.define(version: 2021_02_06_141213) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
